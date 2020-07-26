00999fb4c2a814593bc6e52b6ea22856d8e15bb4
Kyle Zimmer <kylezimmer2@gmail.com>
2020-07-25 21:45:03 -0500

    Content changes - humble start

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid*/
 
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import { Link } from "gatsby";
-import { StaticQuery, graphql } from "gatsby";
-import { HelmetDatoCms } from "gatsby-source-datocms";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import Img from 'gatsby-image';
+import { StaticQuery, graphql } from 'gatsby';
+import { HelmetDatoCms } from 'gatsby-source-datocms';
 
-import "../styles/index.sass";
+import '../styles/index.sass';
 
 const TemplateWrapper = ({ children }) => {
   const [showMenu, setShowMenu] = useState(false);
@@ -31,6 +32,14 @@ const TemplateWrapper = ({ children }) => {
                 html
               }
             }
+            portrait {
+              fluid(
+                maxWidth: 200
+                imgixParams: { fm: "png", auto: "compress" }
+              ) {
+                ...GatsbyDatoCmsSizes
+              }
+            }
             copyright
           }
           allDatoCmsSocialProfile(sort: { fields: [position], order: ASC }) {
@@ -43,62 +52,66 @@ const TemplateWrapper = ({ children }) => {
           }
         }
       `}
-      render={data => (
-        <div className={`container ${showMenu ? "is-open" : ""}`}>
+      render={(data) => (
+        <div className={`container ${showMenu ? 'is-open' : ''}`}>
           <HelmetDatoCms
             favicon={data.datoCmsSite.faviconMetaTags}
             seo={data.datoCmsHome.seoMetaTags}
           />
-          <div className="container__sidebar">
-            <div className="sidebar">
-              <h6 className="sidebar__title">
-                <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-              </h6>
+          <div className='container__sidebar'>
+            <div className='sidebar'>
+              <h1 className='sidebar__title'>
+                <Link to='/'>{data.datoCmsSite.globalSeo.siteName}</Link>
+              </h1>
+              <Img
+                fluid={data.datoCmsHome.portrait.fluid}
+                className='sidebar__portrait'
+              />
               <div
-                className="sidebar__intro"
+                className='sidebar__intro'
                 dangerouslySetInnerHTML={{
                   __html:
-                    data.datoCmsHome.introTextNode.childMarkdownRemark.html
+                    data.datoCmsHome.introTextNode.childMarkdownRemark.html,
                 }}
               />
-              <ul className="sidebar__menu">
+              <ul className='sidebar__menu'>
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link to='/'>Home</Link>
                 </li>
                 <li>
-                  <Link to="/about">About</Link>
+                  <Link to='/about'>About</Link>
                 </li>
               </ul>
-              <p className="sidebar__social">
+              <p className='sidebar__social'>
                 {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
                   <a
                     key={profile.profileType}
                     href={profile.url}
-                    target="blank"
+                    target='blank'
                     className={`social social--${profile.profileType.toLowerCase()}`}
                   >
-                    {" "}
+                    {' '}
                   </a>
                 ))}
               </p>
-              <div className="sidebar__copyright">
+              <div className='sidebar__copyright'>
                 {data.datoCmsHome.copyright}
               </div>
             </div>
           </div>
-          <div className="container__body">
-            <div className="container__mobile-header">
-              <div className="mobile-header">
-                <div className="mobile-header__menu">
+          <div className='container__body'>
+            <div className='container__mobile-header'>
+              <div className='mobile-header'>
+                <div className='mobile-header__menu'>
                   <button
-                    onClick={e => {
+                    onClick={(e) => {
                       e.preventDefault();
                       setShowMenu(!showMenu);
                     }}
                   />
                 </div>
-                <div className="mobile-header__logo">
-                  <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
+                <div className='mobile-header__logo'>
+                  <Link to='/'>{data.datoCmsSite.globalSeo.siteName}</Link>
                 </div>
               </div>
             </div>
@@ -111,7 +124,7 @@ const TemplateWrapper = ({ children }) => {
 };
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.object
+  children: PropTypes.object,
 };
 
 export default TemplateWrapper;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import Masonry from 'react-masonry-component'
-import Img from 'gatsby-image'
-import Layout from "../components/layout"
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Masonry from 'react-masonry-component';
+import Img from 'gatsby-image';
+import Layout from '../components/layout';
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <Masonry className="showcase">
+    <Masonry className='showcase'>
       {data.allDatoCmsWork.edges.map(({ node: work }) => (
-        <div key={work.id} className="showcase__item">
-          <figure className="card">
-            <Link to={`/works/${work.slug}`} className="card__image">
+        <div key={work.id} className='showcase__item'>
+          <figure className='card'>
+            <Link to={`/works/${work.slug}`} className='card__image'>
               <Img fluid={work.coverImage.fluid} />
             </Link>
-            <figcaption className="card__caption">
-              <h6 className="card__title">
+            <figcaption className='card__caption'>
+              <h6 className='card__title'>
                 <Link to={`/works/${work.slug}`}>{work.title}</Link>
               </h6>
-              <div className="card__description">
+              <div className='card__description'>
                 <p>{work.excerpt}</p>
               </div>
             </figcaption>
@@ -26,9 +26,9 @@ const IndexPage = ({ data }) => (
       ))}
     </Masonry>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
 
 export const query = graphql`
   query IndexQuery {
@@ -40,7 +40,7 @@ export const query = graphql`
           slug
           excerpt
           coverImage {
-            fluid(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
+            fluid(maxWidth: 450, imgixParams: { fm: "png", auto: "compress" }) {
               ...GatsbyDatoCmsSizes
             }
           }
@@ -48,4 +48,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
