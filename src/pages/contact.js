ef1f502b3be7a07db1fd6b02fecde9d5cb8f94b3
Kyle Zimmer <kylezimmer2@gmail.com>
2020-08-30 23:06:03 -0500

    Solved errors with contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import { HelmetDatoCms } from 'gatsby-source-datocms';
 import { navigate } from 'gatsby-link';
-import graphql from 'gatsby';
+import { graphql } from 'gatsby';
 
 const encode = (data) => {
   return Object.keys(data)
---
+++ src/pages/thank-you/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+const ThankYou = () => (
+  <Layout>
+    <article className='thankYou'>
+      <div className='thankYou__inner'>
+        <h1 className='thankYou__title'>Thank You!</h1>
+      </div>
+    </article>
+  </Layout>
+);
+
+export default ThankYou;
